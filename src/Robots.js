70a30beeb42a1bab05334f161e8445679196cb4b
Hjkun77 <38162086+Hjkun77@users.noreply.github.com>
2018-05-28 11:50:00 +0800

    Robofriends
    robofriends

---
+++ src/Robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Tony Stark',
+    username: 'Iron Man',
+    email: 'ironman.marvel@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Akigawa Boruto',
+    username: 'Astro Boy',
+    email: 'astro_boy@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Rovert Capricon',
+    username: 'Robo Cop',
+    email: 'robocop@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Sofia Chan',
+    username: 'Socha',
+    email: 'sofiachan_123@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Shania Lim',
+    username: 'Shanwoman',
+    email: 'shana@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Mico Co',
+    username: 'Micco',
+    email: 'MCO@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Marga Diaz',
+    username: 'margarita',
+    email: 'mayor.track_field@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'Lionel Sison',
+    username: 'GOLAZO',
+    email: 'ls.robo@gmail.com'
+  },
+  {
+    id: 9,
+    name: 'Kaithleen Ortiz',
+    username: 'K.O',
+    email: 'kaithortiz@gmail.com'
+  },
+  {
+    id: 10,
+    name: 'Alyanna Perez',
+    username: 'Alyper',
+    email: 'allyperry@gmail.com'
+  }
+]
